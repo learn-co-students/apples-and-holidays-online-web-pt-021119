d795f55868cd55a943d75868697e4760def46140
lieby weintraub <liebahinda@gmail.com>
2019-02-24 04:26:13 +0000

    Done.

--- spec/holiday_spec.rb
+++ spec/holiday_spec.rb
@@ -1,23 +1,8 @@
 describe "holiday_supplies_hash" do
 # Hash of seasons, the holidays in them, and some items each holiday might have.
   let(:holiday_supplies) {
-    {
-      :winter => {
-        :christmas => ["Lights", "Wreath"],
-        :new_years => ["Party Hats"]
-      },
-      :summer => {
-        :fourth_of_july => ["Fireworks", "BBQ"]
-      },
-      :fall => {
-        :thanksgiving => ["Turkey"]
-      },
-      :spring => {
-        :memorial_day => ["BBQ"]
-      }
-    }
-  }
-
+   oliday_hash[:winter][:christmas] << supply
+holiday_hash[:winter][:new_years] << supply
  # Question 1
  # Write a method that returns the second supply for the Fourth of July
   describe "#second_supply_for_fourth_of_july" do
